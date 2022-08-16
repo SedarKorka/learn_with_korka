e355cbaf83d334bf7621b6af0e09643f244d8f8b
Mamadou Korka Dallo <53437647+SedarKorka@users.noreply.github.com>
2022-08-17 02:07:56 +0300

    Add static and url edit

--- maxibas/urls.py
+++ maxibas/urls.py
@@ -13,10 +13,12 @@ urlpatterns = [
     path('api/v1/courses/', include('course.urls')), 
     path('api/v1/activities/', include('activity.urls')), 
     path('summernote/', include('django_summernote.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     
 ]
 #+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
