0b2c8f6d72e216724e075066b55f8cd8a9b1bddf
Mamadou Korka Dallo <53437647+SedarKorka@users.noreply.github.com>
2022-08-17 09:11:51 +0300

    Update settings.py

--- maxibas/settings.py
+++ maxibas/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -74,6 +75,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'maxibas.urls'
+
 
 TEMPLATES = [
     {
